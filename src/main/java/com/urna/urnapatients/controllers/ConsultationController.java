e753129cf96e25a8ab020ad7c5cf081001fdc7f1
admin <admin@Lenovo-PC.domain>
2019-12-30 07:42:39 -0700

    changes in controllers

--- src/main/java/com/urna/urnapatients/controllers/ConsultationController.java
+++ src/main/java/com/urna/urnapatients/controllers/ConsultationController.java
@@ -1,8 +1,18 @@
 package com.urna.urnapatients.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class ConsultationController {
+import com.urna.urnapatients.repo.ConsultationRepository;
 
+@RestController
+@RequestMapping("/rest/urna/consultation")
+public class ConsultationController {
+	ConsultationRepository consultationRepository;
+	
+	@Autowired
+	public void setConsultationRepository(ConsultationRepository consultationRepository) {
+		this.consultationRepository = consultationRepository;
+	}
 }
--- src/main/java/com/urna/urnapatients/controllers/PatientController.java
+++ src/main/java/com/urna/urnapatients/controllers/PatientController.java
@@ -1,8 +1,19 @@
 package com.urna.urnapatients.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.urna.urnapatients.repo.PatientRepository;
+
 @RestController
+@RequestMapping("/rest/urna/patients")
 public class PatientController {
 
+	PatientRepository patientRepository;
+
+	@Autowired
+	public void setPatientRepository(PatientRepository patientRepository) {
+		this.patientRepository = patientRepository;
+	}
 }
