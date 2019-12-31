226e650c53af3f36b535948871f7dc8f2d196591
admin <admin@Lenovo-PC.domain>
2019-12-31 13:56:17 -0700

    updated patient controller

--- src/main/java/com/urna/urnapatients/controllers/PatientController.java
+++ src/main/java/com/urna/urnapatients/controllers/PatientController.java
@@ -1,9 +1,20 @@
 package com.urna.urnapatients.controllers;
 
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.urna.urnapatients.models.Patient;
 import com.urna.urnapatients.repo.PatientRepository;
 
 @RestController
@@ -15,5 +26,37 @@ public class PatientController {
 	@Autowired
 	public void setPatientRepository(PatientRepository patientRepository) {
 		this.patientRepository = patientRepository;
+	}
+	
+	@GetMapping("/patients")
+	public @ResponseBody Iterable<Patient> getAllPatients() {
+	    return patientRepository.findAll();
+	  }
+	
+	@GetMapping("/patient")
+	public @ResponseBody Optional<Patient> getPatientById(@Valid @RequestBody Patient patient) {
+	    return patientRepository.findById(patient.getId());
+	  }
+	
+	
+	
+	@PostMapping("/patient")
+	public Patient createPatient(@Valid @RequestBody Patient patient) {
+	    return patientRepository.save(patient);
+	}
+	
+	@PutMapping("/patient")
+	public Patient updatePatient(@Valid @RequestBody Patient patient) {
+	    return patientRepository.save(patient);
+	}
+	
+	@DeleteMapping("/patient")
+	public void deletePatient(@Valid @RequestBody Patient patient) {
+		patientRepository.delete(patient);
+	}
+	
+	@DeleteMapping("/patient/id")
+	public void deleteDoctorById(@Valid @RequestBody Patient patient) {
+		patientRepository.deleteById(patient.getId());;
 	}
 }
