0baba9ef3743773212e2ae8655eb2e8abfc2d26b
admin <admin@169.254.0.108>
2020-03-02 00:04:44 -0700

    u

--- src/main/java/com/urna/urnapatients/models/Consultation.java
+++ src/main/java/com/urna/urnapatients/models/Consultation.java
@@ -16,6 +16,7 @@ public class Consultation implements java.io.Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
 
 	@Id 
 	@GeneratedValue(strategy = GenerationType.AUTO)
